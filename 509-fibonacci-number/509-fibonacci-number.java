03262360da502d76f1d2b9d440665990b0f59b39
Khushboo Agrawal <55482826+khushbooagrawal245@users.noreply.github.com>
2022-07-26 11:27:34 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (20.11%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public int fib(int n) {
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return func(dp,n);
-    }
-    public static int func(int[] dp,int n){
-        if(n<=1)return n;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n] = func(dp,n-1)+func(dp,n-2);
+        if(n==0 || n==1)return n;
+       int prev = 1;
+        int prev2 = 0;
+        int curr =0;
+        for(int i=2;i<=n;i++){
+            curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return curr;
     }
 }
