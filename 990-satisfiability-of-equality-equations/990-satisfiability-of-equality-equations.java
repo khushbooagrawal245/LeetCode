5b9a8ef7b934191673fe9a972d0a0beab342fc1d
Khushboo Agrawal <55482826+khushbooagrawal245@users.noreply.github.com>
2022-09-27 10:27:51 -0700

    Time: 1 ms (100.00%), Space: 42.4 MB (54.99%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.java
@@ -1,40 +1,25 @@
 class Solution {
-    int[] parent = new int[26];
-    public int find(int x) {
-        // with path compression
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-        // without path compression
-        // return parent[x] == x ? x : find(parent[x]);
-    }
-    // the idea is to put all characters in the same group if they are equal
-    // in order to do that, we can use Disjoint Set Union (dsu) aka Union Find
-    // for dsu tutorial, please check out https://wingkwong.github.io/leetcode-the-hard-way/tutorials/graph-theory/disjoint-set-union
+    int uf[];
     public boolean equationsPossible(String[] equations) {
-        int n = equations.length;
-        // at the beginning, put each character index in its own group
-        // so we will have 26 groups with one character each
-        // i.e. 'a' in group 0, 'b' in group 1, ..., 'z' in group 25
-        for (int i = 0; i < 26; i++) parent[i] = i;
-        for (String e : equations) {
-            // if two character is equal, 
-            if (e.charAt(1) == '=') {
-                // e.g. a = b
-                // then we group them together
-                // how? we use `find` function to find out the parent group of the target character index
-                // then update parent. a & b would be in group 1 (i.e. a merged into the group where b belongs to)
-                // or you can also do `parent[find(e.charAt(3)- 'a')] = find(e.charAt(0) - 'a');` (i.e. b merged into the group where a belongs to)
-                parent[find(e.charAt(0)- 'a')] = find(e.charAt(3) - 'a');
-            }
+        
+         uf = new int[26];
+        for(int i=0;i<26;i++)uf[i]=i;
+        for(String s: equations)if(s.charAt(1)=='='){
+            int p1 = find(s.charAt(0)-'a');
+            int p2 = find(s.charAt(3)-'a');
+            
+            if(p1!=p2) uf[p2]=p1;
         }
-        // handle != case
-        for (String e : equations) {
-            // if two characters are not equal
-            // then which means their parent must not be equal
-            if (e.charAt(1) == '!' && find(e.charAt(0) - 'a') == find(e.charAt(3) - 'a')) {
-                return false;
-            }
+        for(String s: equations)if(s.charAt(1)=='!'){
+            int p1 = find(s.charAt(0)-'a');
+            int p2 = find(s.charAt(3)-'a');
+            
+            if(p1==p2) return false;
         }
         return true;
+        
+    }
+    public int find(int x){
+        return uf[x]==x?x:(uf[x]=find(uf[x]));
     }
 }
